5daa697b3ea5d64b7d3683d8fcb553eda5417037
gnebie <guillaumelale@gmail.com>
2025-02-27 08:43:22 +0100

    deploy change env work

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,3 @@
-console.log("Hello ............")
-console.log("DEBUG ENV:", import.meta.env)
-
 const config = {
   backendChatUrl: import.meta.env.VITE_CHAT_BACKEND_URL || "http://localhost:5000/chat",
   backendEmailUrl: import.meta.env.VITE_MAIL_BACKEND_URL || "http://localhost:5000/mail",
