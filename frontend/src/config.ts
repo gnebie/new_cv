7f1ce23f4e026ebc616b9373b6f5d664a72b25a8
gnebie <guillaumelale@gmail.com>
2025-02-27 02:44:07 +0100

    test deploy change

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,6 @@
+console.log("Hello ............")
+console.log("DEBUG ENV:", import.meta.env)
+
 const config = {
   backendChatUrl: import.meta.env.VITE_CHAT_BACKEND_URL || "http://localhost:5000/chat",
   backendEmailUrl: import.meta.env.VITE_MAIL_BACKEND_URL || "http://localhost:5000/mail",
